83cf6cbd24c2222c4377ca18b04f00ac8a120fbf
gulozge <gulumozge23@gmail.com>
2023-05-08 19:55:18 +0300

    07-05-2023

--- filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
@@ -1,4 +1,4 @@
-package com.kodlamaio.inventoryservice;
+package com.kodlamaio.filterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class FilterServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InventoryServiceApplication.class, args);
+        SpringApplication.run(FilterServiceApplication.class, args);
     }
 
 }
